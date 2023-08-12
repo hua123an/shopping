d1df25d068a2029b9ccd31e218867623a719d7f6
hua123an <1994843581@qq.com>
2023-08-12 13:37:14 +0800

    clear unused file and settings

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
-import './assets/main.css'
-
+import "element-plus/dist/index.css"
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import ElementPlus from 'element-plus'
 
 import App from './App.vue'
 import router from './router'
@@ -10,5 +10,6 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(ElementPlus)
 
 app.mount('#app')
